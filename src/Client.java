e9f50f2ce8c3b2bfe3b442a1530d51e39861cb6d
huyfr <61008601+huyfr@users.noreply.github.com>
2020-04-20 16:58:34 +0700

    Thực hành triển khai facade pattern

---
+++ src/Client.java
@@ -0,0 +1,6 @@
+public class Client {
+    public static void main(String[] args) {
+        RandomListFacade randomListFacade = new RandomListFacade(50,5,100);
+        randomListFacade.printRandomEvenList();
+    }
+}
